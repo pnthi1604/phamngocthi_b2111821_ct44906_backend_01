f31b5ceac8c896a33c711616c86d496757ad7252
phamngocthi <pnthi.it.cp@gmail.com>
2024-01-01 21:38:40 +0700

    update

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app.js');
-const config = require('./app/config');
-
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
